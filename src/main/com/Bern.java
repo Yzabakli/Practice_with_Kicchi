eb1ef5cfae1717bb4a830b198b2b8213b87e32f6
Yzabakli <abakliyusufziya@gmail.com>
2022-08-31 17:32:29 +0200

    modified Geneva class

--- src/main/com/Bern.java
+++ src/main/com/Bern.java
@@ -1,5 +1,6 @@
 package main.com;
 
+import java.math.BigDecimal;
 import java.util.Set;
 
 public class Bern extends Cenevre{
@@ -7,5 +8,13 @@ public class Bern extends Cenevre{
     @Override
     public Set<Character> meth(char[] array) {
         return null;
+    }
+
+    void meth2(int number) {
+
+    }
+    @Override
+    void meth2() {
+        super.meth2();
     }
 }
--- src/main/com/Cenevre.java
+++ src/main/com/Cenevre.java
@@ -1,10 +1,14 @@
 package main.com;
 
+import java.math.BigDecimal;
 import java.util.Set;
 
 public abstract class Cenevre {
 
+    BigDecimal dec;
     public abstract Set<Character> meth(char[] array);
 
-
+    void meth2(){
+        dec = dec.add(dec);
+    }
 }
