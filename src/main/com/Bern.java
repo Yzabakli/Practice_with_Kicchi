3076c11a5148373ab4edd7e96653c654f0f27ac1
Yzabakli <abakliyusufziya@gmail.com>
2022-08-31 17:28:38 +0200

    modified Geneva class

---
+++ src/main/com/Bern.java
@@ -0,0 +1,11 @@
+package main.com;
+
+import java.util.Set;
+
+public class Bern extends Cenevre{
+
+    @Override
+    public Set<Character> meth(char[] array) {
+        return null;
+    }
+}
---
+++ src/main/com/Cenevre.java
@@ -0,0 +1,10 @@
+package main.com;
+
+import java.util.Set;
+
+public abstract class Cenevre {
+
+    public abstract Set<Character> meth(char[] array);
+
+
+}
