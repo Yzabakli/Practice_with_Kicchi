6f3ce803873ba903b7ae8e1ec3da2d0b680e9e77
Yzabakli <abakliyusufziya@gmail.com>
2022-08-31 17:24:02 +0200

    added Geneva class

---
+++ src/main/java/com/cydeo/Geneva.java
@@ -0,0 +1,32 @@
+package com.cydeo;
+
+import java.math.BigDecimal;
+import java.util.LinkedHashMap;
+
+public class Geneva {
+
+    private Integer id;
+    private LinkedHashMap<BigDecimal, Boolean> map;
+    protected static String str;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public LinkedHashMap<BigDecimal, Boolean> getMap() {
+        return map;
+    }
+
+    public Geneva(Integer id){
+
+        setId(id);
+    }
+
+    private void setId(Integer id) {
+        if (id < -18){
+
+            throw new RuntimeException();
+
+        }else this.id = id;
+    }
+}
